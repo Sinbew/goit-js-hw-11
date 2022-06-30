24c92844389f03db7144a74c0bd5b50fd822a11b
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-06-30 12:03:13 +0300

    upd

---
+++ src/api/pixabayApi.js
@@ -0,0 +1,32 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
+export default function fetchImages(value, page) {
+  return axios({
+    url: `https://pixabay.com/api/`,
+    params: {
+      key: '28348938-0384dcc8789dbce7d9ed883a2',
+      q: value,
+      orientation: 'horizontal',
+      image_type: 'photo',
+      safesearch: true,
+      page: page,
+      per_page: 40,
+    },
+  });
+  // url: `https://pixabay.com/api/?key=28348938-0384dcc8789dbce7d9ed883a2&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`;
+  //   const imagesUrl = 'https://pixabay.com/api/';
+  //   const KEY = '?key=28348938-0384dcc8789dbce7d9ed883a2';
+  //   const filter = '&image_type=photo&orientation=horizontal&safesearch=true';
+
+  //   const response = fetch(`${imagesUrl}${KEY}&q=${value}${filter}`)
+  //     .then(data => {
+  //       if (!data.ok) {
+  //         console.log(data);
+  //         throw new Error('Error');
+  //       }
+  //       return data.json();
+  //     })
+  //     .catch(() => Notiflix.Notify.failure('Oops, there is no such image!'));
+  //   return response;
+}
